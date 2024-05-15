67a81e409a1d716bde824471e040396820fb725e
amoniw <walkeramoni@yahoo.com>
2024-05-14 23:58:51 -0400

    first commit

--- src/main/java/Moped.java
+++ src/main/java/Moped.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Moped {
-}
+package com.pluralsight;
+
+public class Moped extends Vehicle {
+    private int maxWeight;
+
+    public int getMaxWeight() {
+        return maxWeight;
+    }
+
+    public void setMaxWeight(int maxWeight) {
+        this.maxWeight = maxWeight;
+    }
+
+    public void ride() {
+        System.out.println("Riding the moped.");
+    }
--- src/main/java/Semitruck.java
+++ src/main/java/Semitruck.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Semitruck {
-}
+package com.pluralsight;
+
+public class SemiTruck extends Vehicle {
+    private int numberOfTrailers;
+
+    public int getNumberOfTrailers() {
+        return numberOfTrailers;
+    }
+
+    public void setNumberOfTrailers(int numberOfTrailers) {
+        this.numberOfTrailers = numberOfTrailers;
+    }
+
+    public void loadCargo() {
+        System.out.println("Cargo loaded onto the truck.");
+    }
+
+    public void unloadCargo() {
+        System.out.println("Cargo unloaded from the truck.");
+    }
